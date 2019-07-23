222ed600ecac9b915031fe2479a2df2d219097d1
vinhle <vinhle@mit.edu>
2019-07-23 04:33:07 -0700

    Add closest mastery champ to level up

--- league api stuff.py
+++ league api stuff.py
@@ -5,49 +5,83 @@ import ast
 import datetime
 import string
 
-#WHICH CHAMPS ARE CLOSE TO LEVELING UP IN MASTERY, days until freeweek change
+#WHICH CHAMPS ARE CLOSE TO LEVELING UP IN MASTERY.
 today = datetime.date.today()
 print(today)
 uppercase = string.ascii_letters
 with open("./id2champ.txt","r") as c:
 	champdata=ast.literal_eval(c.read())
+api_key = "RGAPI-d223b125-cf28-4a26-9a7a-5bd76fbd787b"
 #UPDATE APIKEY
+def generate_json(url):
+	req = requests.get(url)
+	return req.json()
 class Summoner():
 	def __init__(self,summonerName,api_key):
 		### import json of summoner info
 		self.ign,self.key = summonerName,api_key
 		i=f"https://na1.api.riotgames.com//lol/summoner/v4/summoners/by-name/{self.ign}?api_key={self.key}"
-		req = requests.get(i)
-		summonerdata = req.json()
+		self.summonerjson = generate_json(i)
 		#print(summonerdata)
 		### save some important variables
-		self.lvl = summonerdata['summonerLevel']
-		self.accID = summonerdata['accountId']
-		self.encryptedID = summonerdata['id']
+		self.lvl = self.summonerjson['summonerLevel']
+		self.accID = self.summonerjson['accountId']
+		self.encryptedID = self.summonerjson['id']
 		### import json of summoners champion data
 		j = f"https://na1.api.riotgames.com//lol/champion-mastery/v4/champion-masteries/by-summoner/{self.encryptedID}?api_key={self.key}"
-		req = requests.get(j)
-		self.champsplayed = req.json()
+		self.champjson = generate_json(j)
 	def update_key(self,newkey):
 		self.key = newkey
+	def champ(self, func, x=5):	
+		def get_chest(self):
+			print(self.champjson)
+			self.chestable = []
+			for c in self.champjson:
+				if c['chestGranted']!=True:
+					self.chestable.append(champdata[str(c['championId'])])
+			return self.ign+" can get a chest by playing as: " + ', '.join(self.chestable)
+		###
+		def topX(self,x):
+			result = f"{self.ign}'s most played champions are:\n"
+			for i in range(x):
+				key = (self.champjson[i]['championId'])
+				name = champdata[str(key)]
+				points = self.champjson[i]['championPoints']
+				masterylevel = self.champjson[i]['championLevel']
+				result+=f"{i+1}) {name}: Mastery Level {masterylevel}, with {points} mastery points\n"
+			return result
+		###
+		def masteryup(self,x):
+			lvl = {1:[],2:[],3:[],4:[],5:[],6:[]}
+			#print(self.champjson)
+			toreturn=""
+			smallest=[]
+			small=[0,9999]
+			for c in self.champjson:
+				if c['championLevel']!=7: lvl[c['championLevel']].append([champdata[str(c['championId'])],c['championPointsUntilNextLevel'],c['championId'],c['tokensEarned']])
+			for j in list(lvl.keys()):
+				big=[0,0,0,0]
+				if j==5 or j==6:
+					for i in lvl[j]:
+						if i[3]>big[3]: big=i
+					smallest.append(big)
+					continue
+				for i in lvl[j]:
+					if i[1]<small[1]: small=i
+				smallest.append(small)
+				small=[0,9999]
+			for i in range(6):
+				if i<=3:
+					toreturn+=f"The Mastery Level {i+1} champion closest to leveling up is {smallest[i][0]}. You need to get {smallest[i][1]} more mastery points.\n"
+				else:
+					if i==4: toreturn+=f"The Mastery Level {i+1} champion closest to leveling up is {smallest[i][0]}. You need {2-smallest[i][3]} more tokens.\n"
+					else: toreturn+=f"The Mastery Level {i+1} champion closest to leveling up is {smallest[i][0]}. You need {3-smallest[i][3]} more tokens.\n"
 
-	def get_chest(self):
-		#print(self.champsplayed)
-		self.chestable = []
-		for c in self.champsplayed:
-			if c['chestGranted']!=True:
-				self.chestable.append(champdata[str(c['championId'])])
-		return self.ign+" can get a chest by playing as: " + ', '.join(self.chestable)
+			return toreturn+"You can earn about 500 points per game, Mastery 6 tokens are earned with at least an S-, Mastery 7 with at least an S."
+		if func == "get_chest": return get_chest(self)
+		elif func == "topX": return topX(self,x)
+		elif func =="masteryup": return masteryup(self,x)
 
-	def topX(self,x):
-		result = f"{self.ign}'s most played champions are:\n"
-		for i in range(x):
-			key = (self.champsplayed[i]['championId'])
-			name = champdata[str(key)]
-			points = self.champsplayed[i]['championPoints']
-			masterylevel = self.champsplayed[i]['championLevel']
-			result+=f"{i+1}) {name}: Mastery Level {masterylevel}, with {points} mastery points\n"
-		return result
 
 	def rankdata(self,func):
 		def winr8(self):
@@ -59,8 +93,7 @@ class Summoner():
 			returnstring = f"Veteran: {self.rankdata['veteran']} ||| Inactive: {self.rankdata['inactive']} ||| Just Arrived: {self.rankdata['freshBlood']} ||| Winstreak: {self.rankdata['hotStreak']}"
 			return returnstring
 		i = f'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{self.encryptedID}?api_key={self.key}'
-		req = requests.get(i)
-		self.rankdata = (req.json())[0]
+		self.rankdata = (generate_json(i))[0]
 		#print(self.rankdata)
 		que=self.rankdata['queueType']
 		queuetype = que.replace('_',' ')
@@ -78,9 +111,8 @@ class Summoner():
 
 	def freeweek(self):
 		i = f"https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key={self.key}"
-		req = requests.get(i)
-		self.freerotdata = req.json()
-		daysuntiltuesday = (2-today.weekday())%6
+		self.freerotdata = generate_json(i)
+		daysuntiltuesday = (1-today.weekday())%7
 		if daysuntiltuesday==0: daysuntiltuesday = 7
 		daysuntiltuesday = f"\nThere are {daysuntiltuesday} more days until a new rotation."
 		if self.lvl>10:
@@ -91,27 +123,9 @@ class Summoner():
 			rotation = self.freerotdata['freeChampionIdsForNewPlayers']
 			champrot = [champdata[str(i)] for i in rotation]
 			return "These are the champions that are free to play this week: "+', '.join(champrot)+"."+daysuntiltuesday
-api_key ="RGAPI-b613071a-9803-4d6b-8634-5bcdf2e09984"
 Vinh = Summoner("Vinhabust",api_key)
+#print(Vinh.champ("get_chest"))
+print(Vinh.champ("masteryup"))
 #print(Vinh.get_chest())
-print(Vinh.freeweek())
-#print(Vinh.topX(1))
-#print(Vinh.rankdata('other'))
-#yohan = Summoner("Y0H0N3Y",api_key)
-#print(yohan.topX(1))
-'''
-Nolan = Summoner("nowin REE",api_key)
-print(Nolan.mastery())
-
-Carl = Summoner("ReaperDreams",api_key)
-print(Carl.mastery())
-
-KevinN = Summoner("Kaeeede",api_key)
-print(KevinN.mastery())
-
-Sushi = Summoner("DUCK FONALD",api_key)
-print(Sushi.mastery())
-
-Manny = Summoner("Manny810",api_key)
-print(Manny.mastery())
-'''
+#print(Vinh.freeweek())
+#print(Vinh.rankdata('rank'))
